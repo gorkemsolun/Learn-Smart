dd03bdba0e06a53d471fa71e1295ca002a2bfabc
gorkemsolun <gorkemkadir14@hotmail.com>
2024-10-24 17:11:38 +0300

    Chat sidebar improvements #53
    - Added create chat icon
    - Improved fonts
    - Added dropdown menu for course selection

--- frontend/app/course/[course_id]/instructor/[chat_id]/chat-sidebar.tsx
+++ frontend/app/course/[course_id]/instructor/[chat_id]/chat-sidebar.tsx
@@ -1,3 +1,10 @@
+import { Icons } from "@/components/icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
   Sidebar,
   SidebarContent,
@@ -7,9 +14,11 @@ import {
   SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
+  SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { FaChevronDown } from "react-icons/fa";
 
 export function ChatSidebar() {
   const {
@@ -25,14 +34,35 @@ export function ChatSidebar() {
   return (
     <Sidebar>
       <SidebarHeader />
-      <SidebarContent>
+      <SidebarContent className="p-1">
         <SidebarGroup>
-          <SidebarGroupLabel>Chats</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
+          <div className="flex content-center justify-between">
+            <SidebarGroupLabel className="text-xl font-semibold text-slate-100">
+              Chats
+            </SidebarGroupLabel>
+
+            <SidebarMenuButton className="size-8">
+              <Icons.createChat />
+            </SidebarMenuButton>
+          </div>
+
+          <SidebarGroupContent className="font-normal">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  Select Workspace
+                  <FaChevronDown className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+                <DropdownMenuItem>
+                  <span>Course 1</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <SidebarMenu className="flex-col pl-4 ">
               <SidebarMenuItem>Chat 1</SidebarMenuItem>
-              <SidebarMenuItem>Chat 2</SidebarMenuItem>
-              <SidebarMenuItem>Chat 3</SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
