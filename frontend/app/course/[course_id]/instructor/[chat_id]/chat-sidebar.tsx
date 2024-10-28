99d151f197c384bd8dcbb3e4c8412655679b34e8
gorkemsolun <gorkemkadir14@hotmail.com>
2024-10-28 20:07:14 +0300

    Chat sidebar timetable #53

--- frontend/app/course/[course_id]/instructor/[chat_id]/chat-sidebar.tsx
+++ frontend/app/course/[course_id]/instructor/[chat_id]/chat-sidebar.tsx
@@ -9,6 +9,12 @@ import {
 import { FaEllipsisH } from "react-icons/fa";
 
 import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
   Popover,
   PopoverContent,
   PopoverTrigger,
@@ -43,6 +49,51 @@ export function ChatSidebar(chatSidebarParameters: ChatSidebarParameters) {
     isMobile,
     toggleSidebar,
   } = useSidebar();
+
+  function SidebarChat(parameters: { name: string }) {
+    return (
+      <SidebarMenuItem>
+        <div className="flex place-content-center place-items-center justify-between">
+          <span>{parameters.name}</span>
+
+          {/* Popover ellipsis */}
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="ghost">
+                <FaEllipsisH />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-fit flex-col place-content-center place-items-center justify-center align-middle">
+              <div>
+                <Button variant="ghost">Create a quiz</Button>
+              </div>
+              <div>
+                <Button variant="ghost">Create a flashcard</Button>
+              </div>
+              <div>
+                <Button variant="ghost">Delete</Button>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </SidebarMenuItem>
+    );
+  }
+
+  function SidebarTimetableItem(parameters: { name: string }) {
+    return (
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>{parameters.name}</AccordionTrigger>
+          <AccordionContent>
+            <SidebarChat name="chat1" />
+            <SidebarChat name="chat1" />
+            <SidebarChat name="chat1" />
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    );
+  }
 
   return (
     <Sidebar variant="floating">
@@ -80,29 +131,10 @@ export function ChatSidebar(chatSidebarParameters: ChatSidebarParameters) {
             </DropdownMenu>
 
             <SidebarMenu className="flex-col pl-4 ">
-              <SidebarMenuItem>
-                <div className="flex place-content-center place-items-center justify-between">
-                  <span>Chat 1</span>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button variant="ghost">
-                        <FaEllipsisH />
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-fit flex-col place-content-center place-items-center justify-center align-middle">
-                      <div>
-                        <Button variant="ghost">Create a quiz</Button>
-                      </div>
-                      <div>
-                        <Button variant="ghost">Create a flashcard</Button>
-                      </div>
-                      <div>
-                        <Button variant="ghost">Delete</Button>
-                      </div>
-                    </PopoverContent>
-                  </Popover>
-                </div>
-              </SidebarMenuItem>
+              <SidebarTimetableItem name="Today" />
+              <SidebarTimetableItem name="Yesterday" />
+              <SidebarTimetableItem name="Last week" />
+              <SidebarTimetableItem name="Last month" />
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
