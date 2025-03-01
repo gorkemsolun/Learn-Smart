639c125f6b0a027b10ab86f77125a2574571a55b
gorkemsolun <gorkemkadir14@hotmail.com>
2025-03-01 12:05:47 +0300

    Add tests for SignUp component to validate form inputs and handle API interactions

--- frontend/app/sign-up/page.tsx
+++ frontend/app/sign-up/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { backendAPI } from "@/environment/backend_api";
-import { useRouter } from "next/navigation";
-import * as React from "react";
+import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
-import { useToast } from "@/hooks/use-toast";
-import {ToastAction} from "@/components/ui/toast";
 import {
   Card,
   CardContent,
@@ -14,12 +10,21 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import {Icons} from "@/components/icons";
-import {EnvelopeClosedIcon, LockClosedIcon, PersonIcon, CrossCircledIcon, CheckCircledIcon} from "@radix-ui/react-icons";
+import { ToastAction } from "@/components/ui/toast";
+import { backendAPI } from "@/environment/backend_api";
+import { useToast } from "@/hooks/use-toast";
+import {
+  CheckCircledIcon,
+  CrossCircledIcon,
+  EnvelopeClosedIcon,
+  LockClosedIcon,
+  PersonIcon,
+} from "@radix-ui/react-icons";
+import { useRouter } from "next/navigation";
+import * as React from "react";
 import { useState } from "react";
 
 export default function SignUp() {
-
   const [username, setUsername] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -56,7 +61,7 @@ export default function SignUp() {
       return;
     }
 
-    if(!passwordsMatch()) {
+    if (!passwordsMatch()) {
       toast({
         title: "Passwords do not match",
         description: "Please enter the same password",
@@ -105,13 +110,13 @@ export default function SignUp() {
     <main className="flex min-h-screen items-center justify-center">
       <Card className="relative flex h-[72vh] w-3/5 overflow-auto">
         <Button
-          onClick={() => router.push('/sign-in')}
-          className="absolute right-4 top-4 bg-transparent text-foreground shadow-none hover:bg-foreground/10"
+          onClick={() => router.push("/sign-in")}
+          className="text-foreground hover:bg-foreground/10 absolute right-4 top-4 bg-transparent shadow-none"
         >
           Sign in
         </Button>
 
-        <div className="border-1 relative flex w-1/2 items-center justify-center rounded-l-lg bg-foreground/5 p-4">
+        <div className="border-1 bg-foreground/5 relative flex w-1/2 items-center justify-center rounded-l-lg p-4">
           <div className="absolute left-4 top-4 flex items-center space-x-2">
             <Icons.logo className="size-6" />
             <p className="font-bold">edux/ai</p>
@@ -128,54 +133,52 @@ export default function SignUp() {
               <div className="grid w-5/6 items-center gap-4">
                 <div className="flex w-full flex-col space-y-1.5">
                   <div className="relative w-full">
-                    <EnvelopeClosedIcon
-                        className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-gray-400"/>
+                    <EnvelopeClosedIcon className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-gray-400" />
                     <Input
-                        type="text"
-                        placeholder="email@example.com"
-                        className="pl-10"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                      type="text"
+                      placeholder="email@example.com"
+                      className="pl-10"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
                     />
                   </div>
 
                   <div className="relative w-full">
-                    <PersonIcon className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-gray-400"/>
+                    <PersonIcon className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-gray-400" />
                     <Input
-                        type="text"
-                        placeholder="username"
-                        className="pl-10"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
+                      type="text"
+                      placeholder="username"
+                      className="pl-10"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
                     />
                   </div>
 
                   <div className="relative w-full">
-                    <LockClosedIcon
-                        className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-gray-400"/>
+                    <LockClosedIcon className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-gray-400" />
                     <Input
-                        type="password"
-                        placeholder="********"
-                        className="pl-10"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                      type="password"
+                      placeholder="********"
+                      className="pl-10"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
                     />
                   </div>
 
                   <div className="relative w-full">
                     {passwordsMatch() ? (
-                        <CheckCircledIcon
-                            className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-gray-400"/>
+                      <CheckCircledIcon className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-gray-400" />
                     ) : (
-                        <CrossCircledIcon
-                            className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-gray-400"/>
+                      <CrossCircledIcon className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-gray-400" />
                     )}
                     <Input
-                        type="password"
-                        placeholder="********"
-                        className="pl-10"
-                        value={confirmPassword}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setConfirmPassword(e.target.value)}
+                      type="password"
+                      placeholder="********"
+                      className="pl-10"
+                      value={confirmPassword}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setConfirmPassword(e.target.value)
+                      }
                     />
                   </div>
                 </div>
@@ -187,11 +190,19 @@ export default function SignUp() {
           </CardContent>
 
           <CardFooter className="flex w-full justify-center">
-            <p className="w-3/5 text-center text-xs text-foreground/60">
-              By clicking continue, you agree to our <a
-                className="text-foreground/60 underline hover:text-foreground/80" href="">
-            Terms of Service
-              </a> and <a className="text-foreground/60 underline hover:text-foreground/80" href="">
+            <p className="text-foreground/60 w-3/5 text-center text-xs">
+              By clicking continue, you agree to our{" "}
+              <a
+                className="text-foreground/60 hover:text-foreground/80 underline"
+                href=""
+              >
+                Terms of Service
+              </a>{" "}
+              and{" "}
+              <a
+                className="text-foreground/60 hover:text-foreground/80 underline"
+                href=""
+              >
                 Privacy Policy.
               </a>
             </p>
