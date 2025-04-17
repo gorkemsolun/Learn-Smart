066413ba44c546819b977dc8d8f5468ed81aa2d6
gorkemsolun <gorkemkadir14@hotmail.com>
2025-04-17 17:26:44 +0300

    remove old frontend

--- old-frontend/app/constants.ts
+++
@@ -1,6 +0,0 @@
-export const documentMimeTypes = [
-  "application/pdf",
-  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-];
-
-export const imageMimeTypes = ["image/jpeg", "image/png"];
