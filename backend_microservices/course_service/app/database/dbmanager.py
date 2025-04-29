02e5dd1515924dd39adcf6d5ad452cd4e1fa3198
gorkemsolun <gorkemkadir14@hotmail.com>
2025-04-30 01:03:57 +0300

    Fix condition checks for syllabus, icon, and study plan FIDs in update method

--- backend_microservices/course_service/app/database/dbmanager.py
+++ backend_microservices/course_service/app/database/dbmanager.py
@@ -161,17 +161,17 @@ class CourseDB:
         if course_description is not None:
             course.course_description = course_description
 
-        if course_syllabus_fid is "-1":
+        if course_syllabus_fid == "-1":
             course.course_syllabus_fid = None
         elif course_syllabus_fid is not None:
             course.course_syllabus_fid = course_syllabus_fid
 
-        if course_icon_fid is "-1":
+        if course_icon_fid == "-1":
             course.course_icon_fid = None
         elif course_icon_fid is not None:
             course.course_icon_fid = course_icon_fid
 
-        if course_study_plan_fid is "-1":
+        if course_study_plan_fid == "-1":
             course.course_study_plan_fid = None
         elif course_study_plan_fid is not None:
             course.course_study_plan_fid = course_study_plan_fid
