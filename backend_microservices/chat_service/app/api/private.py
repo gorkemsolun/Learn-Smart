6aa400077dcd504c7a14f09fc242b12f820c288d
Cahit Ediz Civan <edizcivan@192.168.0.112>
2025-04-29 23:21:35 +0300

    added private endpoint for getting all chat histories of a course

--- backend_microservices/chat_service/app/api/private.py
+++ backend_microservices/chat_service/app/api/private.py
@@ -1,3 +1,6 @@
+from http.client import HTTPException
+import json
+from chat_service.app.model import ChatHistory
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
 
@@ -52,3 +55,42 @@ async def delete_chats(course_id: int,
 
     ChatDB.delete(db, course_id=course_id, all=True)
     return {"status": "success"}
+
+@router.get("/chat-histories/{course_id}")
+async def get_all_chat_histories_of_course(course_id: int,
+                       db: Session = Depends(get_db)):
+    
+    chats = ChatDB.fetch(db, course_id=course_id, all=True)
+    all_histories = [] #ChatHistory list
+    for chat in chats:
+        if chat["slides_mode"]:
+            history_fids = []
+            slides = SlideDB.fetch(db, chat_id=chat.id, all=True)
+            slide_ids = [slide["slide_id"] for slide in slides]
+
+            for slide_id in slide_ids:
+                pages = SlidePageDB.fetch(db, slide_id=slide_id, all=True)
+                history_fids.extend([page["chat_history_fid"] for page in pages])
+
+            if len(history_fids) == 0:
+                raise HTTPException(status_code=400, detail="No messages found in the chat history to generate quiz.")
+            
+            history_fids = sorted(history_fids)
+            histories = []
+            for history_fid in history_fids:
+                history_bytes = await filemanager.download(file_id=history_fid)
+                history = ChatHistory.from_bytes(history_bytes)
+                histories.append(history)
+
+            history = ChatHistory.merge(histories)
+            all_histories.append(history)
+
+        else:
+            history_fid = chat["history_fid"]
+            if not history_fid:
+                raise HTTPException(status_code=400, detail="No chat history found to generate quiz.")
+            history_bytes = await filemanager.download(file_id=history_fid)
+            history = ChatHistory.from_bytes(history_bytes)
+            all_histories.append(history)
+
+    return {"status": "success", "data": json.dumps([h.google() for h in all_histories])}
