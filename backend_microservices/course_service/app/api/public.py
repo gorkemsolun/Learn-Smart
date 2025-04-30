6f091df3216a9386827f5761a767188718cde6e2
gorkemsolun <gorkemkadir14@hotmail.com>
2025-04-30 20:08:50 +0300

    Remove redundant assignment of new file IDs in update_course function

--- backend_microservices/course_service/app/api/public.py
+++ backend_microservices/course_service/app/api/public.py
@@ -214,7 +214,6 @@ async def update_course(course_id: int, course_name: Optional[str] = Form(None),
         try:
             if old_icon_id:
                 await filemanager.delete(course["course_icon_fid"])  # delete old image
-                new_icon_fid = "-1"
         except Exception as e:
             error = True
             error_message = str(e)
@@ -238,11 +237,9 @@ async def update_course(course_id: int, course_name: Optional[str] = Form(None),
         try:
             if old_syllabus_id:
                 await filemanager.delete(course["course_syllabus_fid"])  # delete old syllabus
-                new_syllabus_fid = "-1"
             
             if old_study_plan_id:
                 await filemanager.delete(course["course_study_plan_fid"])  # delete old study plan
-                new_study_plan_fid = "-1"
         except Exception as e:
             error = True
             error_message = str(e)
